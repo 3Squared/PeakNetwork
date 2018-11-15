fe145c67e1554b27b5ff713ca7294d8ea701eed3
Zack Brown <zack.brown@3squared.com>
2018-11-15 15:05:00 +0000

    Refactor network folder structure

--- PeakNetwork/Core/PeakNetwork.h
+++ PeakNetwork/Core/PeakNetwork.h
@@ -6,7 +6,15 @@
 //  Copyright © 2017 3Squared. All rights reserved.
 //
 
+#if TARGET_OS_IPHONE
+
 #import <UIKit/UIKit.h>
+
+#elif TARGET_OS_MAC
+
+#import <Cocoa/Cocoa.h>
+
+#endif
 
 //! Project version number for PeakNetwork.
 FOUNDATION_EXPORT double PeakNetworkVersionNumber;
